70c65994d6578a5842a9c8ca4ee5a98456d249f1
Duc <mintduc2612@gmail.com>
2023-10-10 23:42:38 +0700

    ductm

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,38 +23,39 @@ class AuthController extends Controller
     public function register(Request $request){
         try{
             $validator = Validator::make($request->all(),
-        [
-         'fullname'=>['required', 'regex:/^[\p{L}\p{M}\p{Pd}\p{Zs}\']+$/u'],
-         'username' => ['required', 'regex:/^[a-zA-Z0-9]+$/'],
-         'password'=> ['required', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/'],
-         'email'=>['required','email'],
-         'day_of_birth'=>['required'],
-         'gender'=>['required|string'],
-        ]
-        );
-        if ($validator->fails()) {
-            // Xử lý khi có lỗi trong validator
-            return response()->error($validator->errors(), 400);
-        }
+                [
+                    'fullname'=>['required', 'regex:/^[\p{L}\p{M}\p{Pd}\p{Zs}\']+$/u'],
+                    'username' => ['required', 'regex:/^[a-zA-Z0-9]+$/'],
+                    'password'=> ['required', 'regex:/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/'],
+                    'email'=>['required','email'],
+                    'day_of_birth'=>['required'],
+                    'gender'=>['required|string'],
+                ]
+            );
+            if ($validator->fails()) {
+                // Xử lý khi có lỗi trong validator
+                return response()->error($validator->errors(), 400);
+            }
 
-        // $accountModel = new Account();
-        if($this->account->checkDuplicate($request->input('username'), $request->input('email'))){
-            return response()->error('email hoặc username đã tồn tại.',400);
-        }
+            // $accountModel = new Account();
+            if($this->account->checkDuplicate($request->input('username'), $request->input('email'))){
+                return response()->error('email hoặc username đã tồn tại.',400);
+            }
 
-        $account = Account::create([
-            'fullname' => $request->input('fullname'),
-            'username' => $request->input('username'),
-            'password' => Hash::make($request->input('password')),
-            'email' => $request->input('email'),
-            'day_of_birth' => $request->input('day_of_birth'),
-            'gender' => $request->input('gender'),
-        ]);
-        if(!$account){
-            return response()->error('Không thể tạo tài khoản.', 500);
-        }
-        return response()->success($account, 'Tài khoản đã được tạo thành công.', 200);
+            $account = Account::create([
+                'fullname' => $request->input('fullname'),
+                'username' => $request->input('username'),
+                'password' => Hash::make($request->input('password')),
+                'email' => $request->input('email'),
+                'day_of_birth' => $request->input('day_of_birth'),
+                'gender' => $request->input('gender'),
+            ]);
 
+            if(!$account){
+                return response()->error('Không thể tạo tài khoản.', 500);
+            }
+
+            return response()->success($account, 'Tài khoản đã được tạo thành công.', 200);
         }catch(Exception $ex){
             throw $ex;
         }
@@ -103,5 +104,43 @@ class AuthController extends Controller
         });
 
         return response()->json(['message' => 'Logout successful']);
+    }
+
+    public function show($image)
+    {
+        $path = storage_path('app/public/media/' . $image);
+        
+        if (!File::exists($path)) {
+            abort(404);
+        }
+        
+        $file = File::get($path);
+        $type = File::mimeType($path);
+
+        $response = response($file, 200)->header("Content-Type", $type);
+
+        return $response;
+    }
+
+    public function uploadFile(Request $request){
+        try {
+           // up ảnh
+           $imageInfo = array();
+           if ($request->hasFile('media')) {
+               $images = $request->file('media');
+               foreach ($images as $image) {
+                   $originalName = $image->getClientOriginalName();
+                   $extension = $image->getClientOriginalExtension();
+                   $randomString = uniqid();
+                   $imageName = time() . '' . $originalName . '' . $randomString . '.' . $extension;
+                   $image->move(public_path('storage/media'), $imageName);
+                   $imageInfo[] = ['type' => $image->getClientOriginalExtension(),'name' => $imageName];
+               }
+               return response()->success(['file_info' => $imageInfo],'Tải lên ảnh thành công',200);
+           }
+           return response()->error('Tải lên ảnh thất bại !',400);
+        } catch (Throwable $th) {
+            throw $th;
+        }
     }
 }
